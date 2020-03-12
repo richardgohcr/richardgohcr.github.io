a561094dd371866aec10a8165e8d8b7cb73b4ab0
richardgohcr <richard.gohcr@gmail.com>
2020-03-13 02:55:26 +0800

    Added responsive navbar version 1 for tablets

--- js/app.js
+++ js/app.js
@@ -78,3 +78,27 @@ navLinks.forEach(link => {
     link.style.color = '#00ADEF';
   });
 });
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  //Toggle nav bar
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //Animate links
+    navLinks.forEach((link, index) => {
+      //Dividing index by a number will give us a nice proprtional delay for each li
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.3}s`;
+      }
+    });
+    burger.classList.toggle('toggle');
+  });
+};
+
+navSlide();
+
