3a580cbf75427471babb81616287a98a6bcd364d
richardgohcr <richard.gohcr@gmail.com>
2020-03-14 13:12:30 +0800

    Fixed nav links animation

--- js/app.js
+++ js/app.js
@@ -80,12 +80,13 @@ navLinks.forEach(link => {
 });
 
 const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
   //Toggle nav bar
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
+  burger.addEventListener('click', () => {
+    console.log('navslide');
+    nav.classList.toggle('nav-active');
 
     //Animate links
     navLinks.forEach((link, index) => {
@@ -93,7 +94,8 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.3}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.3}s`;
       }
     });
     burger.classList.toggle('toggle');
@@ -102,3 +104,25 @@ const navSlide = () => {
 
 navSlide();
 
+const selectLink = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  navLinks.forEach(link =>
+    link.addEventListener('click', () => {
+      burger.classList.toggle('toggle');
+      nav.classList.toggle('nav-active');
+      console.log(link.style.animation);
+      //Remove link animations
+
+      setTimeout(() => {
+        navLinks.forEach(link => {
+          link.style.animation = '';
+        });
+      }, 500);
+    }),
+  );
+};
+
+selectLink();
